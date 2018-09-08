470b9386fdf96d7c9897060e2795eede4b2ee231
chanchal <chanchalnerkar@gmail.com>
2018-09-08 22:45:36 +0200

    Crud operations added

---
+++ src/main/java/com/restfully/shop/config/ShoppingApplication.java
@@ -0,0 +1,29 @@
+package com.restfully.shop.config;
+
+import com.restfully.shop.services.CustomerResourceService;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+import java.util.HashSet;
+import java.util.Set;
+
+@ApplicationPath("/services")
+public class ShoppingApplication extends Application {
+
+    private Set<Object> singletons = new HashSet<>();
+    private Set<Class<?>> empty = new HashSet<>();
+
+    public ShoppingApplication() {
+        singletons.add(new CustomerResourceService());
+    }
+
+    @Override
+    public Set<Class<?>> getClasses() {
+        return empty;
+    }
+
+    @Override
+    public Set<Object> getSingletons() {
+        return singletons;
+    }
+}
--- src/main/java/com/restfully/shop/services/CustomerResource.java
+++ src/main/java/com/restfully/shop/services/CustomerResource.java
@@ -17,115 +17,21 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.PrintStream;
-import java.net.URI;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.atomic.AtomicInteger;
 
-/**
- *
- */
 @Path("/customers")
-public class CustomerResource {
-
-    private Map<Integer, Customer> customerDB = new ConcurrentHashMap<>();
-    private AtomicInteger idCounter = new AtomicInteger();
-
+public interface CustomerResource {
 
     @POST
     @Consumes(MediaType.APPLICATION_XML)
-    public Response createCustomer(InputStream is){
-        Customer customer = readCustomer(is);
-        customer.setId(idCounter.incrementAndGet());
-        customerDB.put(customer.getId(),customer);
-        System.out.println("Created Customer : "+customer.getId());
-        return Response.created(URI.create("/customers/"+customer.getId())).build();
-    }
+    Response createCustomer(InputStream is);
 
     @GET
     @Path("{id}")
     @Produces(MediaType.APPLICATION_XML)
-    public StreamingOutput getCustomer(@PathParam("id") int id){
-        final Customer customer = customerDB.get(id);
-        if(customer == null){
-            new WebApplicationException(Response.Status.NOT_FOUND);
-        }
-        return new StreamingOutput() {
-            @Override
-            public void write(OutputStream output) throws IOException, WebApplicationException {
-                outputCustomer(output, customer);
-            }
-        };
-    }
+    StreamingOutput getCustomer(@PathParam("id") int id);
 
     @PUT
     @Path("{id}")
     @Consumes(MediaType.APPLICATION_JSON)
-    public void updateCustomer( @PathParam("id") int id, InputStream is){
-        Customer customer = readCustomer(is);
-        if(customerDB.get(id) == null){
-            throw new WebApplicationException(Response.Status.NOT_FOUND);
-        }
-        customerDB.put(id, customer);
-
-    }
-
-    private Customer readCustomer(InputStream is) {
-        DocumentBuilder builder = null;
-        Customer cust = null ;
-
-        try {
-            builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
-            Document doc = builder.parse(is);
-            Element root = doc.getDocumentElement();
-            cust = new Customer();
-            if(root.getAttribute("id") != null  && !root.getAttribute("id").trim().equals("")){
-                cust.setId(Integer.valueOf(root.getAttribute("id")));
-            }
-
-            NodeList nodes = root.getChildNodes();
-            for (int i = 0 ;i< nodes.getLength();i++){
-                Element element = (Element) nodes.item(i);
-                if(element.getTagName().equals("first-name")){
-                    cust.setFirstName(element.getTextContent());
-                }else if (element.getTagName().equals("last-name")){
-                    cust.setLastName(element.getTextContent());
-                }else if (element.getTagName().equals("street")){
-                    cust.setStreet(element.getTextContent());
-                }else if (element.getTagName().equals("city")){
-                    cust.setCity(element.getTextContent());
-                }else if (element.getTagName().equals("state")){
-                    cust.setState(element.getTextContent());
-                }else if (element.getTagName().equals("zip")){
-                    cust.setZip(element.getTextContent());
-                }else if (element.getTagName().equals("country")){
-                    cust.setCountry(element.getTextContent());
-                }
-            }
-
-        } catch (SAXException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ParserConfigurationException e) {
-            e.printStackTrace();
-        }
-        return cust;
-    }
-
-    private void outputCustomer(OutputStream os, Customer cust){
-        PrintStream writer = new PrintStream(os);
-        writer.println("<customer id=\"" + cust.getId() + "\">");
-        writer.println("<first-name>" + cust.getFirstName()
-                + "</first-name>");
-        writer.println("<last-name>" + cust.getLastName()
-                + "</last-name>");
-        writer.println("<street>" + cust.getStreet() + "</street>");
-        writer.println("<city>" + cust.getCity() + "</city>");
-        writer.println("<state>" + cust.getState() + "</state>");
-        writer.println("<zip>" + cust.getZip() + "</zip>");
-        writer.println("<country>" + cust.getCountry() + "</country>");
-        writer.println("</customer>");
-    }
-
+    void updateCustomer( @PathParam("id") int id, InputStream is);
 }
